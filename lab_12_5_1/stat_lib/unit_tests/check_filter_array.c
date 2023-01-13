a26d81db0a56dd1ae0e3ad0ac64de3e35e918b4d
Mamvriyskiy <m_ivan_s@mail.ru>
2023-01-13 13:35:05 +0300

    end stat_lib

---
+++ lab_12_5_1/stat_lib/unit_tests/check_filter_array.c
@@ -0,0 +1,337 @@
+#include <stdio.h>
+#include <check.h>
+#include <stdlib.h>
+#include "key.h"
+#include "check_main.h"
+#include "ext_error.h"
+
+//Не найдены элементы, удовлетворяющие условие
+START_TEST(test_count_correct_element_no_search)
+{
+    int n = 6;
+    long long int suml = 12330;
+    int p[] = {1, 2, 3, 4, 5, 12345};
+    size_t count = 0, result_count = 0;
+    count = count_correct_element(p, p + n, suml);
+    ck_assert_uint_eq(count, result_count);
+}
+END_TEST
+
+//Простой поиск количества элементов
+START_TEST(test_count_correct_element_usual_search)
+{
+    int n = 6;
+    long long int suml = -12330;
+    int p[] = {1, 2, 3, 4, 5, -12345};
+    size_t count = 0, result_count = 5;
+    count = count_correct_element(p, p + n, suml);
+    ck_assert_uint_eq(count, result_count);
+}
+END_TEST
+
+//Поиск количества элементов в массиве, содержащем похожие числа
+START_TEST(test_count_correct_element_array_with_same_element)
+{
+    int n = 6;
+    long long int suml = -1;
+    int p[] = {0, 0, 0, 0, 0, -1};
+    size_t count = 0, result_count = 5;
+    count = count_correct_element(p, p + n, suml);
+    ck_assert_uint_eq(count, result_count);
+}
+END_TEST
+
+//Значение суммы равно 0
+START_TEST(test_create_sum_zero_suml)
+{
+    int n = 6;
+    long long int suml = 0;
+    long long int suml_result = 0;
+    int p[] = {100, 200, 500, -650, -150, 0};
+    suml = create_sum(p, p + n);
+    ck_assert_int_eq(suml, suml_result);
+}
+END_TEST
+
+//Поиск суммы в массиве, состоящем из одинаковых элементов
+START_TEST(test_create_sum_array_with_same_element)
+{
+    int n = 6;
+    long long int suml = 0;
+    long long int suml_result = 600;
+    int p[] = {100, 100, 100, 100, 100, 100};
+    suml = create_sum(p, p + n);
+    ck_assert_int_eq(suml, suml_result);
+}
+END_TEST
+
+//Поиск суммы в массиве, длина которого равна 1
+START_TEST(test_create_sum_array_len_one)
+{
+    int n = 1;
+    long long int suml = 0;
+    long long int suml_result = 55;
+    int p[] = {55};
+    suml = create_sum(p, p + n);
+    ck_assert_int_eq(suml, suml_result);
+}
+END_TEST
+
+//Простой поиск элементов
+START_TEST(test_search_right_element_usual_search)
+{
+    int n = 5;
+    long long int suml = -8;
+    int arr_1[] = {55, 34, 2, -45, -54};
+    int arr_2[4] = { 0 };
+    int result_arr[] = {55, 34, 2, -45};
+    search_right_element(arr_1, arr_1 + n, arr_2, suml);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Поиск элементов в массиве, содержащем похожие числа
+START_TEST(test_search_right_element_array_with_zero)
+{
+    int n = 5;
+    long long int suml = -8;
+    int arr_1[] = {0, 0, 0, 0, -1};
+    int arr_2[4] = { 0 };
+    int result_arr[] = {0, 0, 0, 0};
+    search_right_element(arr_1, arr_1 + n, arr_2, suml);
+    int error = cmp_array(arr_2, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Найден только один элемент
+START_TEST(test_search_right_element_array_with_one_right_result)
+{
+    int n = 5;
+    long long int suml = 2011;
+    int arr_1[] = {1234, 23, 54, 200, 500};
+    int arr_2[1] = { 0 };
+    int result_arr[] = {1234};
+    search_right_element(arr_1, arr_1 + n, arr_2, suml);
+    int error = cmp_array(arr_2, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Поиск элементов в массиве, который содержит только положительные числа
+START_TEST(test_key_array_with_only_positive_element)
+{
+    int rc = OK;
+
+    int arr1_b[] = {35, 64, 235, 634, 43, 62, 35, 3124, 324};
+    int *arr1_e = arr1_b + 9;
+
+    int *pb_dst = NULL;
+    int *pe_dst = NULL;
+
+    int arr2[] = {3124};
+    int len2 = 1;
+
+    rc = key(arr1_b, arr1_e, &pb_dst, &pe_dst);
+
+    if (!rc)
+    {
+        long len1 = pe_dst - pb_dst;
+
+        if (len1 != len2)
+            rc = NEGATIVE_SIZE_ARRAY;
+        else
+            rc = cmp_array(pb_dst, arr2, sizeof(arr2));
+    }
+
+    free(pb_dst);
+
+    ck_assert_int_eq(rc, OK);
+}
+END_TEST
+
+//Поиск элементов в массиве, который содержит похожие числа
+START_TEST(test_key_array_with_zero)
+{
+    int rc = OK;
+    int arr1_b[] = {0, 0, 0, 0, 0, -1};
+    int *arr1_e = arr1_b + 6;
+
+    int *pb_dst = NULL;
+    int *pe_dst = NULL;
+
+    int arr2[] = {0, 0, 0, 0, 0};
+    int len2 = 5;
+
+    key(arr1_b, arr1_e, &pb_dst, &pe_dst);
+
+    if (!rc)
+    {
+        long len1 = pe_dst - pb_dst;
+
+        if (len1 != len2)
+            rc = NEGATIVE_SIZE_ARRAY;
+        else
+            rc = cmp_array(pb_dst, arr2, sizeof(arr2));
+    }
+
+
+    free(pb_dst);
+
+    ck_assert_int_eq(rc, OK);
+}
+END_TEST
+
+//Поиск элементов в массиве, который содержит только отрицательные числа
+START_TEST(test_key_array_with_only_negative_element)
+{
+    int rc = OK;
+    int arr1_b[] = {-12, -45, -1, -34, -5};
+    int *arr1_e = arr1_b + 5;
+
+    int *pb_dst = NULL;
+    int *pe_dst = NULL;
+
+    int arr2[] = {-12, -1};
+    int len2 = 2;
+
+    rc = key(arr1_b, arr1_e, &pb_dst, &pe_dst);
+
+    if (!rc)
+    {
+        long len1 = pe_dst - pb_dst;
+
+        if (len1 != len2)
+            rc = NEGATIVE_SIZE_ARRAY;
+        else
+            rc = cmp_array(pb_dst, arr2, sizeof(arr2));
+    }
+
+
+    free(pb_dst);
+
+    ck_assert_int_eq(rc, OK);
+}
+END_TEST
+
+//Поиск элементов в массиве, который содержит разные числа
+START_TEST(test_key_array_with_mixed_element)
+{
+    int rc = OK;
+    int arr1_b[] = {-12, 2, -1, 0, -5};
+    int *arr1_e = arr1_b + 5;
+
+    int *pb_dst = NULL;
+    int *pe_dst = NULL;
+
+    int arr2[] = {2, -1, 0};
+    int len2 = 3;
+
+    rc = key(arr1_b, arr1_e, &pb_dst, &pe_dst);
+
+    if (!rc)
+    {
+        long len1 = pe_dst - pb_dst;
+
+        if (len1 != len2)
+            rc = NEGATIVE_SIZE_ARRAY;
+        else
+            rc = cmp_array(pb_dst, arr2, sizeof(arr2));
+    }
+
+    free(pb_dst);
+
+    ck_assert_int_eq(rc, OK);
+}
+END_TEST
+
+//Проверка на находждение элементов в массиве, длина которого равна 1
+START_TEST(test_key_array_with_one_element)
+{
+    int arr1_b[] = {1};
+    int *arr1_e = arr1_b + 1;
+
+    int *pb_dst = NULL;
+    int *pe_dst = NULL;
+
+    int rc = key(arr1_b, arr1_e, &pb_dst, &pe_dst);
+
+    free(pb_dst);
+
+    ck_assert_int_eq(rc, 7);
+}
+END_TEST
+
+//Элементы в массиве не найдены
+START_TEST(test_key_no_search_element)
+{
+    int arr1_b[] = {34, 12, -1, 34, 1234};
+    int *arr1_e = arr1_b + 5;
+
+    int *pb_dst = NULL;
+    int *pe_dst = NULL;
+
+    int rc = key(arr1_b, arr1_e, &pb_dst, &pe_dst);
+
+    free(pb_dst);
+
+    ck_assert_int_eq(rc, 7);
+}
+END_TEST
+
+//Передача нулевого указателя
+START_TEST(test_key_null)
+{
+    int *arr1_b = NULL;
+    int *arr1_e = NULL;
+
+    int *pb_dst = NULL;
+    int *pe_dst = NULL;
+
+    int rc = key(arr1_b, arr1_e, &pb_dst, &pe_dst);
+
+    free(pb_dst);
+
+    ck_assert_int_eq(rc, 6);
+}
+END_TEST
+
+Suite *filter_suite()
+{
+    Suite *s;
+    TCase *tc_neg;
+    TCase *tc_pos;
+
+    s = suite_create("count_correct_element");
+
+    tc_neg = tcase_create("negatieves");
+    tcase_add_test(tc_neg, test_key_array_with_one_element);
+    tcase_add_test(tc_neg, test_key_no_search_element);
+    tcase_add_test(tc_neg, test_key_null);
+    suite_add_tcase(s, tc_neg);
+
+    tc_pos = tcase_create("positieves");
+    //count_correct_element
+    tcase_add_test(tc_pos, test_count_correct_element_no_search);
+    tcase_add_test(tc_pos, test_count_correct_element_usual_search);
+    tcase_add_test(tc_pos, test_count_correct_element_array_with_same_element);
+    //create_sum
+    tcase_add_test(tc_pos, test_create_sum_zero_suml);
+    tcase_add_test(tc_pos, test_create_sum_array_with_same_element);
+    tcase_add_test(tc_pos, test_create_sum_array_len_one);
+    //search_right_element
+    tcase_add_test(tc_pos, test_search_right_element_usual_search);
+    tcase_add_test(tc_pos, test_search_right_element_array_with_zero);
+    tcase_add_test(tc_pos, test_search_right_element_array_with_one_right_result);
+    //key
+    tcase_add_test(tc_pos, test_key_array_with_zero);
+    tcase_add_test(tc_pos, test_key_array_with_only_positive_element);
+    tcase_add_test(tc_pos, test_key_array_with_only_negative_element);
+    tcase_add_test(tc_pos, test_key_array_with_mixed_element);
+
+    suite_add_tcase(s, tc_pos);
+
+    return s;
+}
+
---
+++ lab_12_5_1/stat_lib/unit_tests/check_sort_array.c
@@ -0,0 +1,252 @@
+#include <stdio.h>
+#include <check.h>
+#include "sorted.h"
+#include "check_main.h"
+#include "ext_error.h"
+
+//Меняет местами элементы типа int
+START_TEST(test_swap_el_normal_swap_one)
+{
+    int first_el = 34;
+    int second_el = 1234;
+
+    swap_el(&first_el, &second_el, sizeof(first_el));
+
+    ck_assert_int_eq(first_el, 1234);
+    ck_assert_int_eq(second_el, 34);
+}
+END_TEST
+
+//Меняет местами элементы типа int
+START_TEST(test_swap_el_normal_swap_second)
+{
+    int first_el = -1;
+    int second_el = 34;
+
+    swap_el(&first_el, &second_el, sizeof(first_el));
+
+    ck_assert_int_eq(first_el, 34);
+    ck_assert_int_eq(second_el, -1);
+}
+END_TEST
+
+//Меняет местами элементы типа double
+// START_TEST(test_swap_el_double_swap)
+// {
+//    double first_el = -0.1;
+//    double second_el = 34.65;
+
+//    swap_el(&first_el, &second_el, sizeof(first_el));
+
+//    ck_assert_double_eq_tol(first_el, 34.65, EPS);
+//    ck_assert_double_eq_tol(second_el, -0.1, EPS);
+// }
+// END_TEST
+
+//Меняет местами элементы типа char
+START_TEST(test_swap_el_char_swap)
+{
+    char first_el[] = "34";
+    char second_el[] = "24";
+
+    swap_el(&first_el, &second_el, sizeof(first_el));
+
+    ck_assert_str_eq(first_el, "24");
+    ck_assert_str_eq(second_el, "34");
+}
+END_TEST
+
+//Обыкновенное сравнение элементов
+START_TEST(test_compare_normal_one)
+{
+    int first_el = 12;
+    int second_el = -1;
+
+    int a = compare(&first_el, &second_el);
+    ck_assert_int_eq(a, 1);
+}   
+END_TEST
+
+//Обыкновенное сравнение элементов
+START_TEST(test_compare_normal_second)
+{
+    int first_el = 34;
+    int second_el = 1234;
+
+    int a = compare(&first_el, &second_el);
+    ck_assert_int_eq(a, -1);
+}
+END_TEST
+
+//Обыкновенное сравнение элементов
+START_TEST(test_compare_normal_third)
+{
+    int first_el = 34;
+    int second_el = 34;
+
+    int a = compare(&first_el, &second_el);
+    ck_assert_int_eq(a, 0);
+}
+END_TEST
+
+//Сортировка одного элемента
+START_TEST(test_mysort_one_element)
+{
+    int n = 1;
+    int arr_1[] = {55};
+    int result_arr[] = {55};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка упорядоченного по возрастанию массива
+START_TEST(test_mysort_array_ascending_order)
+{
+    int n = 6;
+    int arr_1[] = {1, 2, 3, 4, 5, 6};
+    int result_arr[] = {1, 2, 3, 4, 5, 6};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка упорядоченного по убыванию массива
+START_TEST(test_mysort_array_descending_order)
+{
+    int n = 6;
+    int arr_1[] = {6, 5, 4, 3, 2, 1};
+    int result_arr[] = {1, 2, 3, 4, 5, 6};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка массива, содержащего повторяющиеся элементы
+START_TEST(test_mysort_array_with_identical_elements)
+{
+    int n = 6;
+    int arr_1[] = {1, 2, 3, 3, 2, 1};
+    int result_arr[] = {1, 1, 2, 2, 3, 3};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка упорядоченного по возрастанию массива типа double
+START_TEST(test_mysort_array_ascending_order_double)
+{
+    int n = 6;
+    double arr_1[] = {1.0, 2.5, 3.1, 4.9, 5.0, 6.1};
+    double result_arr[] = {1.0, 2.5, 3.1, 4.9, 5.0, 6.1};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare_double);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка упорядоченного по убыванию массива типа double
+START_TEST(test_mysort_array_descending_order_double)
+{
+    int n = 6;
+    double arr_1[] = {6.1, 5, 4.9, 3.1, 2.5, 1.0};
+    double result_arr[] = {1.0, 2.5, 3.1, 4.9, 5, 6.1};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare_double);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка массива типа double
+START_TEST(test_mysort_array_double)
+{
+    int n = 6;
+    double arr_1[] = {6.0, 5.6, 3.1, 3.0, 14.5, 1.0};
+    double result_arr[] = {1.0, 3.0, 3.1, 5.6, 6.0, 14.5};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare_double);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка упорядоченного по возрастанию массива типа char
+START_TEST(test_mysort_array_ascending_order_char)
+{
+    int n = 6;
+    char arr_1[] = {'1', '2', '3', '4', '5', '6'};
+    char result_arr[] = {'1', '2', '3', '4', '5', '6'};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare_char);
+    int error = cmp_array(arr_1, result_arr, sizeof(arr_1));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка упорядоченного по убыванию массива типа char
+START_TEST(test_mysort_array_descending_order_char)
+{
+    int n = 6;
+    char arr_1[] = {'6', '5', '4', '3', '2', '1'};
+    char result_arr[] = {'1', '2', '3', '4', '5', '6'};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare_char);
+    int error = cmp_array(arr_1, result_arr, sizeof(result_arr));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+//Сортировка массива типа char
+START_TEST(test_mysort_array_char)
+{
+    int n = 6;
+    char arr_1[] = {'5', '2', '4', '3', '6', '1'};
+    char result_arr[] = {'1', '2', '3', '4', '5', '6'};
+    mysort(arr_1, n, sizeof(arr_1[0]), compare_char);
+    int error = cmp_array(arr_1, result_arr, sizeof(arr_1));
+    ck_assert_int_eq(error, OK);
+}
+END_TEST
+
+Suite *sort_suite()
+{
+    Suite *s;
+    TCase *tc_neg;
+    TCase *tc_pos;
+
+    s = suite_create("count_correct_element");
+
+    tc_neg = tcase_create("negatieves");
+    
+    suite_add_tcase(s, tc_neg);
+
+    tc_pos = tcase_create("positieves");
+
+    //swap_el
+    tcase_add_test(tc_pos, test_swap_el_normal_swap_one);
+    tcase_add_test(tc_pos, test_swap_el_normal_swap_second);
+    // tcase_add_test(tc_pos, test_swap_el_double_swap);
+    tcase_add_test(tc_pos, test_swap_el_char_swap);
+
+    //compare
+    tcase_add_test(tc_pos, test_compare_normal_one);
+    tcase_add_test(tc_pos, test_compare_normal_second);
+    tcase_add_test(tc_pos, test_compare_normal_third);
+
+    //mysort
+    tcase_add_test(tc_pos, test_mysort_one_element);
+    tcase_add_test(tc_pos, test_mysort_array_ascending_order);
+    tcase_add_test(tc_pos, test_mysort_array_descending_order);
+    tcase_add_test(tc_pos, test_mysort_array_with_identical_elements);
+    tcase_add_test(tc_pos, test_mysort_array_ascending_order_double);
+    tcase_add_test(tc_pos, test_mysort_array_descending_order_double);
+    tcase_add_test(tc_pos, test_mysort_array_double);
+    tcase_add_test(tc_pos, test_mysort_array_ascending_order_char);
+    tcase_add_test(tc_pos, test_mysort_array_descending_order_char);
+    tcase_add_test(tc_pos, test_mysort_array_char);
+
+    suite_add_tcase(s, tc_pos);
+
+    return s;
+}
